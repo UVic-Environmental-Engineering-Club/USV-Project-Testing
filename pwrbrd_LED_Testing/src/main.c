c42764d5dcf6bb580e59bd7e72baf33193ee6be1
Evan <evanpeters@telus.net>
2022-08-05 13:22:23 -0700

    Update main.c

--- pwrbrd_LED_Testing/src/main.c
+++ pwrbrd_LED_Testing/src/main.c
@@ -1,4 +1,5 @@
 #include "Status_LED.h"
+#include "driver/gpio.h"
 
 #define GREEN   0x0A0000
 #define RED     0x001500
@@ -14,4 +15,11 @@ void app_main() {
   new_state.leds[2] = GREEN;
 
   Status_LED_write(new_state);
+
+  gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+  gpio_set_level(GPIO_NUM_2, 1);
+  gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+  gpio_set_level(GPIO_NUM_4, 1);
+  gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT);
+  gpio_set_level(GPIO_NUM_5, 1);
 }
